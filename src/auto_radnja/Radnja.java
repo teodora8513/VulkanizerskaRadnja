fc21ac14a627c06411c30137c9bce3e7f106cfce
teodora8513 <jevtic.teodora@gmail.com>
2021-03-20 21:44:36 +0100

    Dokumentovan interfejs Radnja

--- src/auto_radnja/Radnja.java
+++ src/auto_radnja/Radnja.java
@@ -2,9 +2,22 @@ package auto_radnja;
 
 import java.util.LinkedList;
 import auto_radnja.gume.AutoGuma;
-
+/**
+ * Interfejs koji predstavlja radnju koja drzi gume
+ * @author Teodora
+ *
+ */
 public interface Radnja {
+	/**
+	 * Dodavanje gume a
+	 * @param a A kao auto guma koja se dodaje
+	 */
 	void dodajGumu(AutoGuma a);
 
+	/**
+	 * Pronalazi gumu odredjene marke
+	 * @param markaModel Marka modela gume kao String
+	 * @return Listu auto guma marke modela
+	 */
 	LinkedList<AutoGuma> pronadjiGumu(String markaModel);
 }
